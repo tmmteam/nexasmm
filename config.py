1ee20ac3eb4aaf4f59ba5b3a6e06747bc2808af5
tmmteam <teamtmm2@gmail.com>
2026-04-14 11:48:56 +0530

    🥀ᴀʏᴜsʜ🥀

--- config.py
+++ config.py
@@ -2,13 +2,17 @@ API_ID = 12345
 API_HASH = "your_hash"
 BOT_TOKEN = "your_token"
 
-ADMIN_ID = 123456789
+ADMIN_ID = 5311223486
+
+CHANNELS = ["@FROZENTOOLS", "@Teamsanki", "@Tmm_smm_world"]
+PRIVATE_CHANNEL = "https://t.me/xxxx"
+
 
 DB_URL = "https://nexasmm-default-rtdb.firebaseio.com"
 
 UPI_ID = "yourupi@upi"
 UPI_NAME = "Ayush"
 
-PAYMENT_CHANNEL = "@yourchannel"
+PAYMENT_CHANNEL = "@NEXASEXAPRO"
 
 WEB_URL = "https://your-site.com"

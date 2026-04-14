acf8cc7cdb847b6972a154e3e091f09ee8b022a3
tmmteam <teamtmm2@gmail.com>
2026-04-14 10:32:26 +0530

    🥀ᴀʏᴜsʜ🥀

---
+++ config.py
@@ -0,0 +1,15 @@
+API_ID = 12345
+API_HASH = "your_hash"
+BOT_TOKEN = "your_token"
+
+ADMIN_ID = 123456789
+
+CHANNELS = ["@FROZENTOOLS", "@Teamsanki", "@Tmm_smm_world"]
+PRIVATE_CHANNEL = "https://t.me/xxxx"
+
+PAYMENT_CHANNEL = "@yourlogchannel"
+
+UPI_ID = "yourupi@upi"
+UPI_NAME = "Ayush"
+
+WEB_APP_URL = "https://example.com"
